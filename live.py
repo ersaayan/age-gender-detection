62de270c2637d0f9ef21ca1db12284765085d223
ersaayan <ersaayan@gmail.com>
2024-03-25 15:32:08 +0300

    Update video capture method

--- live.py
+++ live.py
@@ -47,7 +47,37 @@ gender_input_shape = gender_input_details[0]['shape']
 """class_labels=['Angry','Disgust', 'Fear', 'Happy','Neutral','Sad','Surprise']"""
 gender_labels = ['Male', 'Female']
 
-cap=cv2.VideoCapture(0)
+def gstreamer_pipeline(
+    capture_width=640,
+    capture_height=480,
+    display_width=640,
+    display_height=480,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink drop=True"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
+#cap=cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+
+cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+
 
 while True:
     ret,frame=cap.read()
